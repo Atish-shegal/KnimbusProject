51c555cfe04191424d4711a9e2df5c871616ad13
atishshegal <qaselenium77@gmail.com>
2023-04-08 23:21:48 +0530

    1. Pushed Annotations. 2. Pushed Listeners 3. Modified code for failing Test Case. 4. Enhance Extent Report

---
+++ src/main/java/com/knimbus/listeners/TestListeners.java
@@ -0,0 +1,53 @@
+package com.knimbus.listeners;
+
+import com.knimbus.annotations.FrameworkAnnotation;
+import com.knimbus.reports.ExtentLogger;
+import com.knimbus.reports.ExtentReport;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.util.Arrays;
+
+public class TestListeners implements ITestListener {
+
+    public void onTestStart(ITestResult result) {
+        ExtentReport.createTest(result.getMethod().getDescription());
+        ExtentReport.assignAuthor(result.getMethod().getConstructorOrMethod().getMethod().getAnnotation(FrameworkAnnotation.class).author());
+    }
+
+        public void onTestSuccess(ITestResult result) {
+            ExtentLogger.pass(result.getName() + " is passed");
+    }
+
+
+    public  void onTestFailure(ITestResult result) {
+        ExtentLogger.fail(result.getName() + " is failed");
+        ExtentLogger.fail(result.getThrowable().getMessage());
+        ExtentLogger.fail(Arrays.toString(result.getThrowable().getStackTrace()));
+
+    }
+
+
+    public void onTestSkipped(ITestResult result) {
+        ExtentLogger.info(result.getName() + " is skipped");
+    }
+
+    public  void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        // not implemented
+    }
+
+    public  void onTestFailedWithTimeout(ITestResult result) {
+        onTestFailure(result);
+    }
+
+
+    public void onStart(ITestContext context) {
+        ExtentReport.initReports();
+    }
+
+
+    public void onFinish(ITestContext context) {
+        ExtentReport.flushReports();
+    }
+}
